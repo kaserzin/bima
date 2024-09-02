85b1c1dc2a20c95d2e1a86b9f23e1c9a79ae5d43
kaser <pedro.rogerio@academico.ifpb.edu.br>
2024-09-02 18:02:39 -0300

    Version: 1.5

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import *
-from db import Equipe, Partida, Chave
+from db import Equipe, Chave
 
 app = Flask(__name__)
 
 e = Equipe
-p = Partida
 c = Chave
 
 @app.route('/')
@@ -38,49 +37,30 @@ def editar_equipe(id):
         return redirect('/')
     return render_template('form_equipe.html', equipe=equipe, title='Editar Equipe')
 
-@app.route('/partidas')
-def list_partidas():
-    partidas = p.listar_partidas()
-    return render_template("partidas.html", partidas=partidas)
 
-@app.route("/partidas/remover/<int:id>")
-def apagar_partida(id):
-    p.remove_partida(id)
-    return redirect('/partidas')
+@app.route("/chaves")
+def listar_chaves():
+    chaves = Chave.listar_chaves()
+    for chave in chaves:
+        chave['equipes'] = Chave.listar_equipes_chave(chave['id'])
+    return render_template('chaves.html', chaves=chaves)
 
-@app.route("/partidas/novo", methods=['GET', 'POST'])
-def cadastrar_partida():    
-    if request.method == 'POST':
-        dados = request.form.to_dict()
-        p.nova_partida(dados.get('data'), dados.get('local'))
-        return redirect('/partidas')
-    return render_template('form_partida.html', partida=None, title='Nova Partida')
 
-@app.route("/partidas/editar/<int:id>", methods=['GET', 'POST'])
-def editar_partida(id):
-    if request.method == 'POST':
-        dados = request.form.to_dict()
-        p.atualiza_partida(id, dados.get('data'), dados.get('local'))
-        return redirect('/partidas')
-    partida = p.detalha_partida(id)
-    return render_template('form_partida.html', partida=partida, title='Editar Partida')
-
-@app.route('/chaves')
-def list_chaves():
-    chaves = c.listar_chaves()
-    return render_template("chaves.html", chaves=chaves)
-
-@app.route("/chaves/novo", methods=['GET', 'POST'])
-def cadastrar_chave():
-    if request.method == 'POST':
-        dados = request.form.to_dict()
-        nova_chave_id = c.nova_chave(dados.get('nome'))
+@app.route("/chaves/novo", methods=["GET", "POST"])
+def nova_chave():
+    if request.method == "POST":
+        nome_chave = request.form['nome_chave']
         equipes_selecionadas = request.form.getlist('equipes')
+        nova_chave_id = Chave.nova_chave(nome_chave)
+        
         for equipe_id in equipes_selecionadas:
-            c.adicionar_equipe(nova_chave_id, int(equipe_id))
-        return redirect(f'/chaves/{nova_chave_id}/equipes')
-    equipes = e.listar_equipe()
-    return render_template('form_chave.html', chave=None, title='Nova Chave', equipes=equipes)
+            Chave.adicionar_equipe(nova_chave_id, equipe_id)
+        
+        return redirect(url_for('listar_chaves'))
+    else:
+        todas_equipes = Equipe.listar_equipe()
+        equipes_disponiveis = [equipe for equipe in todas_equipes if Chave.equipes_disponiveis(equipe['id'])]
+        return render_template('form_chave.html', equipes=equipes_disponiveis)
 
 
 
@@ -103,21 +83,25 @@ def remover_equipe_chave(id_chave, id_equipe):
     c.remover_equipe(id_chave, id_equipe)
     return redirect(f'/chaves/{id_chave}/equipes')
 
-@app.route("/chaves/<int:id>/editar", methods=['GET', 'POST'])
-def editar_chave(id):
-    chave = c.detalha_chave(id)
-    if request.method == 'POST':
-        dados = request.form.to_dict()
-        c.atualiza_chave(id, dados.get('nome'))
-        return redirect('/chaves')
-    if chave is None:
-        flash('Chave não encontrada')
-        return redirect('/chaves')
-    return render_template('form_chave.html', chave=chave, title='Editar Chave')
 
-@app.route("/chaves/<int:id>/remover")
-def apagar_chave(id):
-    c.remove_chave(id)
+@app.route('/editar_chave/<int:id>', methods=['GET', 'POST'])
+def editar_chave(id):
+    if request.method == 'POST':
+        pass
+    else:
+        chave = Chave.detalha_chave(id)
+        equipes_na_chave = Chave.listar_equipes_chave(id)
+        equipes_disponiveis = Chave.equipes_disponiveis()
+
+        return render_template('editar_chave.html', title="Editar Chave", chave=chave, equipes_na_chave=equipes_na_chave, equipes_disponiveis=equipes_disponiveis)
+    
+
+
+
+@app.route("/chaves/remover/<int:id>", methods=['GET'])
+def remover_chave(id):
+    chave = Chave.detalha_chave(id)
+    Chave.remove_chave(id)
     return redirect('/chaves')
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -44,8 +44,7 @@ class Equipe:
             UPDATE equipes
             SET nome=?, representante=?
             WHERE id=?;
-            """, (nome, representante, id))
-
+        """, (nome, representante, id))
         conn.commit()
         conn.close()
 
@@ -77,102 +76,6 @@ class Equipe:
         }
 
 
-
-class Partida:
-    def criar():
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS partidas (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                data DATE NOT NULL,
-                local VARCHAR(255) NOT NULL
-            );
-        """)
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS partida_equipe (
-                id_partida INTEGER NOT NULL,
-                id_equipe INTEGER NOT NULL,
-                pontos INTEGER NOT NULL,
-                FOREIGN KEY(id_partida) REFERENCES partidas(id),
-                FOREIGN KEY(id_equipe) REFERENCES equipes(id),
-                PRIMARY KEY (id_partida, id_equipe)
-            );
-        """)
-        conn.close()
-
-    def nova_partida(data, local):
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO partidas (data, local)
-            VALUES (?, ?);
-        """, (data, local))
-        conn.commit()
-        conn.close()
-
-    def listar_partidas():
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        values = cursor.execute("SELECT * FROM partidas")
-        resultado = []
-        for row in values:
-            resultado.append({
-                'id': row[0],
-                'data': row[1],
-                'local': row[2],
-            })
-        conn.close()
-        return resultado
-
-    def atualiza_partida(id, data, local):
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            UPDATE partidas
-            SET data=?, local=?
-            WHERE id=?;
-        """, (data, local, id))
-        conn.commit()
-        conn.close()
-
-    def remove_partida(id):
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            DELETE FROM partidas
-            WHERE id=?;
-        """, (id,))
-        conn.commit()
-        conn.close()
-
-    def detalha_partida(id):
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            SELECT * FROM partidas
-            WHERE id=?;
-        """, (id,))
-        item = cursor.fetchone()
-        conn.close()
-        if item is None:
-            return None
-        return {
-            'id': item[0],
-            'data': item[1],
-            'local': item[2],
-        }
-        
-    def adicionar_equipe(id_partida, id_equipe, pontos):
-        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO partida_equipe (id_partida, id_equipe, pontos)
-            VALUES (?, ?, ?);
-        """, (id_partida, id_equipe, pontos))
-        conn.commit()
-        conn.close()
-        
 class Chave:
     def criar():
         conn = sqlite3.connect('II_Taca_CZ_Volei.db')
@@ -202,9 +105,9 @@ class Chave:
             VALUES (?);
         """, (nome,))
         conn.commit()
-        nova_chave_id = cursor.lastrowid 
+        nova_chave_id = cursor.lastrowid  # Captura o ID da nova chave
         conn.close()
-        return nova_chave_id 
+        return nova_chave_id  # Retorna o ID da chave recém-criada
 
     def listar_chaves():
         conn = sqlite3.connect('II_Taca_CZ_Volei.db')
@@ -239,6 +142,23 @@ class Chave:
         conn.commit()
         conn.close()
 
+    def detalha_chave(id_chave):
+        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
+        cursor = conn.cursor()
+        values = cursor.execute("""
+            SELECT e.id, e.nome FROM chave_equipe ce
+            JOIN equipes e ON ce.id_equipe = e.id
+            WHERE ce.id_chave=?;
+        """, (id_chave,))
+        resultado = []
+        for row in values:
+            resultado.append({
+                'id': row[0],
+                'nome': row[1],
+            })
+        conn.close()
+        return resultado
+    
     def listar_equipes_chave(id_chave):
         conn = sqlite3.connect('II_Taca_CZ_Volei.db')
         cursor = conn.cursor()
@@ -256,23 +176,53 @@ class Chave:
         conn.close()
         return resultado
 
-    def equipes_disponiveis():
+    def equipes_disponiveis(id_equipe=None):
         conn = sqlite3.connect('II_Taca_CZ_Volei.db')
         cursor = conn.cursor()
-        values = cursor.execute("""
-            SELECT id, nome FROM equipes
-            WHERE id NOT IN (SELECT id_equipe FROM chave_equipe);
-        """)
-        resultado = []
-        for row in values:
-            resultado.append({
-                'id': row[0],
-                'nome': row[1],
-            })
-        conn.close()
-        return resultado
 
+        if id_equipe:
+            consulta = """
+                SELECT 1 FROM chave_equipe WHERE id_equipe=?;
+            """
+            cursor.execute(consulta, (id_equipe,))
+            result = cursor.fetchone()
+            conn.close()
+            return result is None
+        else:
+            consulta = """
+                SELECT id, nome FROM equipes WHERE id NOT IN (SELECT id_equipe FROM chave_equipe);
+            """
+            values = cursor.execute(consulta)
+            resultado = [{'id': row[0], 'nome': row[1]} for row in values]
+            conn.close()
+            return resultado
+
+    def atualiza_chave(id_chave, nome):
+        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE chaves
+            SET nome=?
+            WHERE id=?;
+        """, (nome, id_chave))
+        conn.commit()
+        conn.close()
+        
+    def remove_chave(id_chave):
+        conn = sqlite3.connect('II_Taca_CZ_Volei.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM chave_equipe
+            WHERE id_chave=?;
+        """, (id_chave,))
+        cursor.execute("""
+            DELETE FROM chaves
+            WHERE id=?;
+        """, (id_chave,))
+        
+        conn.commit()
+        conn.close()
+        
 if __name__ == '__main__':
     Equipe.criar()
-    Partida.criar()
     Chave.criar()
